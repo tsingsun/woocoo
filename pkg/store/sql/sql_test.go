ae04676be3d6a2f7322c55fe0881e9ceb7595f03
李清山 <21997272@qq.com>
2021-12-23 15:04:50 +0800

    add sql.db tool

---
+++ pkg/store/sql/sql_test.go
@@ -0,0 +1,44 @@
+package sql_test
+
+import (
+	native "database/sql"
+	"database/sql/driver"
+	"github.com/tsingsun/woocoo/pkg/conf"
+	"github.com/tsingsun/woocoo/pkg/store/sql"
+	"testing"
+)
+
+type testDriver struct{}
+
+func (w *testDriver) Open(name string) (driver.Conn, error) {
+	return nil, nil
+}
+
+func TestNewBuiltInDB(t *testing.T) {
+	{
+		native.Register("testDriver", &testDriver{})
+	}
+	config := `
+store:
+  testDriver:
+    driverName: testDriver
+    dsn: root:123456@tcp(localhost:3306)
+`
+	cfg := conf.NewFromBytes([]byte(config))
+	cfg.AsGlobal()
+	type args struct {
+		path string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{name: "testDriver", args: args{path: "testDriver"}, wantErr: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			sql.NewBuiltInDB(tt.args.path)
+		})
+	}
+}

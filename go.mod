a7b6e7f28aaaf756930a68fd6c21ead2b2efc32d
李清山 <tsingsun@live.cn>
2023-08-09 15:45:52 +0800

    feat: add configurable http client (#337)

--- go.mod
+++ go.mod
@@ -29,19 +29,19 @@ require (
 	go.uber.org/goleak v1.2.1
 	go.uber.org/multierr v1.11.0
 	go.uber.org/zap v1.24.0
-	golang.org/x/crypto v0.9.0 // indirect
-	golang.org/x/net v0.10.0
-	golang.org/x/oauth2 v0.6.0
+	golang.org/x/crypto v0.12.0 // indirect
+	golang.org/x/net v0.14.0
+	golang.org/x/oauth2 v0.11.0
 	golang.org/x/sync v0.3.0
-	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/text v0.9.0
+	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/text v0.12.0
 	google.golang.org/grpc v1.55.0
-	google.golang.org/protobuf v1.30.0
+	google.golang.org/protobuf v1.31.0
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1
 )
 
 require (
-	cloud.google.com/go/compute v1.18.0 // indirect
+	cloud.google.com/go/compute v1.20.1 // indirect
 	cloud.google.com/go/compute/metadata v0.2.3 // indirect
 	github.com/Knetic/govaluate v3.0.1-0.20171022003610-9aa49832a739+incompatible // indirect
 	github.com/alicebob/gopher-json v0.0.0-20200520072559-a9ecdc9d1d3a // indirect
@@ -79,6 +79,8 @@ require (
 	go.etcd.io/etcd/client/pkg/v3 v3.5.9 // indirect
 	golang.org/x/arch v0.3.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/genproto v0.0.0-20230306155012-7f2fa6fef1f4 // indirect
+	google.golang.org/genproto v0.0.0-20230530153820-e85fd2cbaebc // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20230530153820-e85fd2cbaebc // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230530153820-e85fd2cbaebc // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- pkg/httpx/client.go
+++ pkg/httpx/client.go
@@ -1,45 +1,80 @@
 package httpx
 
 import (
+	"context"
 	"fmt"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/clientcredentials"
 	"net/http"
 	"net/url"
 	"strings"
+	"time"
 
 	"github.com/tsingsun/woocoo/pkg/conf"
 	"golang.org/x/net/http/httpproxy"
 )
 
-// ClientConfig configures an HTTP client.
-type ClientConfig struct {
-	TransportConfig
-	// The HTTP basic authentication credentials for the targets.
-	BasicAuth *BasicAuth `yaml:"basic_auth,omitempty" json:"basic_auth,omitempty"`
-	// The HTTP authorization credentials for the targets.
-	Authorization *Authorization `yaml:"authorization,omitempty" json:"authorization,omitempty"`
-	// The OAuth2 client credentials used to fetch a token for the targets.
-	OAuth2 *OAuth2 `yaml:"oauth2,omitempty" json:"oauth2,omitempty"`
-}
+type (
+	// ClientConfig configures an HTTP client.
+	ClientConfig struct {
+		TransportConfig
+		Timeout time.Duration `yaml:"timeout" json:"timeout"`
+		// The HTTP basic authentication credentials for the targets.
+		BasicAuth *BasicAuth `yaml:"basicAuth,omitempty" json:"basicAuth,omitempty"`
+		// The HTTP authorization credentials for the targets.
+		Authorization *Authorization `yaml:"authorization,omitempty" json:"authorization,omitempty"`
+		// The OAuth2 client credentials used to fetch a token for the targets.
+		OAuth2 *OAuth2Config `yaml:"oauth2,omitempty" json:"oauth2,omitempty"`
 
-// Authorization contains HTTP authorization credentials.
-type Authorization struct {
-	Type        string `yaml:"type,omitempty" json:"type,omitempty"`
-	Credentials string `yaml:"credentials,omitempty" json:"credentials,omitempty"`
-}
+		base http.RoundTripper
+	}
 
-// BasicAuth contains basic HTTP authentication credentials.
-type BasicAuth struct {
-	Username string `yaml:"username" json:"username"`
-	Password string `yaml:"password,omitempty" json:"password,omitempty"`
-}
+	// Authorization contains HTTP authorization credentials.
+	Authorization struct {
+		Type        string `yaml:"type,omitempty" json:"type,omitempty"`
+		Credentials string `yaml:"credentials,omitempty" json:"credentials,omitempty"`
+	}
 
-// OAuth2 is the oauth2 client configuration.
-type OAuth2 struct {
-	ClientID       string            `yaml:"clientId" json:"clientId"`
-	ClientSecret   string            `yaml:"clientSecret" json:"clientSecret"`
-	Scopes         []string          `yaml:"scopes,omitempty" json:"scopes,omitempty"`
-	TokenURL       string            `yaml:"tokenUrl" json:"tokenUrl"`
-	EndpointParams map[string]string `yaml:"endpointParams,omitempty" json:"endpointParams,omitempty"`
+	// BasicAuth contains basic HTTP authentication credentials.
+	BasicAuth struct {
+		Username string `yaml:"username" json:"username"`
+		Password string `yaml:"password,omitempty" json:"password,omitempty"`
+	}
+
+	OAuth2Config struct {
+		*oauth2.Config
+		EndpointParams url.Values
+	}
+
+	TransportConfig struct {
+		*ProxyConfig `yaml:",inline" json:",inline"`
+		// TLSConfig to use to connect to the targets.
+		TLS *conf.TLS `yaml:"tls,omitempty"`
+	}
+
+	ProxyConfig struct {
+		// HTTP proxy server to use to connect to the targets.
+		ProxyURL string `yaml:"proxyUrl,omitempty" json:"proxyUrl,omitempty"`
+		// NoProxy contains addresses that should not use a proxy.
+		NoProxy string `yaml:"noProxy,omitempty" json:"noProxy,omitempty"`
+		// ProxyConnectHeader optionally specifies headers to send to
+		// proxies during CONNECT requests. Assume that at least _some_ of
+		// these headers are going to contain secrets and use Secret as the
+		// value type instead of string.
+		ProxyConnectHeader http.Header `yaml:"proxyConnectHeader,omitempty" json:"proxyConnectHeader,omitempty"`
+	}
+)
+
+// NewClientConfig creates a new ClientConfig by options.
+func NewClientConfig(opts ...Option) ClientConfig {
+	cfg := ClientConfig{}
+	for _, opt := range opts {
+		opt(&cfg)
+	}
+	if cfg.BasicAuth != nil {
+		cfg.base = chain(cfg.base, BaseAuth(cfg.BasicAuth.Username, cfg.BasicAuth.Password))
+	}
+	return cfg
 }
 
 func (c *ClientConfig) Validate() error {
@@ -53,36 +88,18 @@ func (c *ClientConfig) Validate() error {
 			return fmt.Errorf("authorization type %q no support", c.Authorization.Type)
 		}
 	}
-	if c.OAuth2 != nil {
+	if c.OAuth2 != nil && c.OAuth2.Config != nil {
 		if c.OAuth2.ClientID == "" {
 			return fmt.Errorf("oauth2 clientId must be configured")
 		}
 		if c.OAuth2.ClientSecret == "" {
 			return fmt.Errorf("either oauth2 clientSecret must be configured")
 		}
-		if c.OAuth2.TokenURL == "" {
+		if c.OAuth2.Endpoint.TokenURL == "" {
 			return fmt.Errorf("oauth2 tokenUrl must be configured")
 		}
 	}
 	return nil
-}
-
-type TransportConfig struct {
-	*ProxyConfig `yaml:",inline" json:",inline"`
-	// TLSConfig to use to connect to the targets.
-	TLS *conf.TLS `yaml:"tls,omitempty"`
-}
-
-type ProxyConfig struct {
-	// HTTP proxy server to use to connect to the targets.
-	ProxyURL string `yaml:"proxyUrl,omitempty" json:"proxyUrl,omitempty"`
-	// NoProxy contains addresses that should not use a proxy.
-	NoProxy string `yaml:"noProxy,omitempty" json:"noProxy,omitempty"`
-	// ProxyConnectHeader optionally specifies headers to send to
-	// proxies during CONNECT requests. Assume that at least _some_ of
-	// these headers are going to contain secrets and use Secret as the
-	// value type instead of string.
-	ProxyConnectHeader http.Header `yaml:"proxyConnectHeader,omitempty" json:"proxyConnectHeader,omitempty"`
 }
 
 func (p ProxyConfig) Validate() error {
@@ -106,8 +123,20 @@ func (p ProxyConfig) ProxyFunc() func(req *http.Request) (*url.URL, error) {
 	}
 }
 
+// NewTransport creates a new HTTP transport base on TransportConfig and http.DefaultTransport.
 func NewTransport(cfg TransportConfig) (http.RoundTripper, error) {
-	ts := http.DefaultTransport.(*http.Transport)
+	df := http.DefaultTransport.(*http.Transport)
+	ts := &http.Transport{
+		Proxy:                 df.Proxy,
+		DialContext:           df.DialContext,
+		MaxIdleConns:          df.MaxIdleConns,
+		MaxIdleConnsPerHost:   df.MaxIdleConnsPerHost,
+		DisableKeepAlives:     df.DisableKeepAlives,
+		DisableCompression:    df.DisableCompression,
+		IdleConnTimeout:       df.IdleConnTimeout,
+		TLSHandshakeTimeout:   df.TLSHandshakeTimeout,
+		ExpectContinueTimeout: df.ExpectContinueTimeout,
+	}
 	if cfg.TLS != nil {
 		t, err := cfg.TLS.BuildTlsConfig()
 		if err != nil {
@@ -122,12 +151,42 @@ func NewTransport(cfg TransportConfig) (http.RoundTripper, error) {
 	return ts, nil
 }
 
-func NewClient(base http.RoundTripper, cfg ClientConfig) (c *http.Client, err error) {
-	if base == nil {
-		base, err = NewTransport(cfg.TransportConfig)
+// NewClient creates a new HTTP client.
+//
+// OAuth2 Client from Configuration is use client credentials flow.You can use TokenSource to custom Source.
+func NewClient(cfg ClientConfig) (c *http.Client, err error) {
+	if cfg.base == nil {
+		cfg.base, err = NewTransport(cfg.TransportConfig)
 		if err != nil {
 			return nil, err
 		}
 	}
-	return &http.Client{Transport: base}, nil
+	c = &http.Client{Transport: cfg.base, Timeout: cfg.Timeout}
+	if cfg.OAuth2 != nil {
+		config := &clientcredentials.Config{
+			ClientID:       cfg.OAuth2.ClientID,
+			ClientSecret:   cfg.OAuth2.ClientSecret,
+			Scopes:         cfg.OAuth2.Scopes,
+			TokenURL:       cfg.OAuth2.Endpoint.TokenURL,
+			EndpointParams: cfg.OAuth2.EndpointParams,
+		}
+		hc := &http.Client{Transport: cfg.base}
+		ctx := context.WithValue(context.Background(), oauth2.HTTPClient, hc)
+		c.Transport = &oauth2.Transport{
+			Base:   hc.Transport,
+			Source: config.TokenSource(ctx),
+		}
+
+		return
+	}
+	return
+}
+
+// NewClientFromCnf creates a new HTTP client from config.
+func NewClientFromCnf(cnf *conf.Configuration) (*http.Client, error) {
+	cfg := NewClientConfig(WithConfiguration(cnf))
+	if err := cfg.Validate(); err != nil {
+		return nil, err
+	}
+	return NewClient(cfg)
 }
